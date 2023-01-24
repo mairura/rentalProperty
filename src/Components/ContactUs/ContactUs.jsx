0c03a01d203eebefb5402b7315d526d5a68d6f0f
ted <mairurat@gmail.com>
2023-01-25 00:02:20 +0300

    Contact Form Created

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -2,7 +2,50 @@ import React from "react";
 import "./contactus.css";
 
 const ContactUs = () => {
-  return <div>ContactUs Page</div>;
+  return (
+    <div className="contactus_page">
+      <h3>
+        Your property with us and be Confident that Your Room will be Filled
+        Out!
+      </h3>
+      <div className="contactus_form">
+        <h3>Add A New Property</h3>
+        <form>
+          <label>Name</label>
+          <input type="text" placeholder="Enter Name" />
+          <label>Address</label>
+          <input type="text" placeholder="Address" />
+          <label>Unit Number</label>
+          <input type="text" placeholder="Enter Unit" />
+          <label>City</label>
+          <select>
+            <option value="">Select City</option>
+            <option value="Nairobi">Nairobi</option>
+            <option value="Nakuru">Nakuru</option>
+            <option value="Kajiado">Kajiado</option>
+          </select>
+          <label>State</label>
+          <select>
+            <option value="">Select State</option>
+            <option value="Nairobi">Nairobi</option>
+            <option value="Nakuru">Nakuru</option>
+            <option value="Rongai">Rongai</option>
+          </select>
+          <label>Room Type</label>
+          <select>
+            <option value="">Select Room</option>
+            <option value="Nairobi">1BR</option>
+            <option value="Nakuru">2BR</option>
+            <option value="Kajiado">3BR</option>
+          </select>
+          <label>Price</label>
+          <input type="text" placeholder="Enter Price" />
+          <label>State</label>
+          <input type="text" placeholder="Select State" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
--- src/Components/Tenants/Decor.jsx
+++ src/Components/Tenants/Decor.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 import "./tenants.css";
+import houseA from "../../Assets/houseA.png";
+import houseB from "../../Assets/houseB.png";
+import houseC from "../../Assets/houseC.png";
+import houseD from "../../Assets/houseD.png";
+import houseE from "../../Assets/houseE.png";
+import houseF from "../../Assets/houseF.png";
 
 const Decor = () => {
+  const houses = [
+    {
+      name: "Kilimani Super Large",
+      desc: "Private Room",
+      price: "Ksh30000/night",
+      image: houseA,
+    },
+    {
+      name: "Kilimani Super Large",
+      desc: "Private Room",
+      price: "Ksh30000/night",
+      image: houseB,
+    },
+    {
+      name: "Kilimani Super Large",
+      desc: "Private Room",
+      price: "Ksh30000/night",
+      image: houseC,
+    },
+  ];
   return (
     <div>
       <div className="properties_container">
-        <h3>List of Properties</h3>
-        <div className="properties"></div>
+        <div className="header">
+          <div className="property_head">
+            <h3>List of Properties</h3>
+          </div>
+          <div>
+            <button>View All Property</button>
+          </div>
+        </div>
+        <div className="properties">
+          {houses.map((house, index) => (
+            <div key={index}>
+              {/* <div>{house.image}</div> */}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
