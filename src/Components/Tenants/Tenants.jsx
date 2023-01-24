f2cf415ab530aab9f559b437c619047016477c29
ted <mairurat@gmail.com>
2023-01-24 17:53:40 +0300

    About Page

--- src/Components/Tenants/Tenants.jsx
+++ src/Components/Tenants/Tenants.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import "./tenants.css";
+import Vector from "../../Assets/Vector.png";
+import House from "../../Assets/sample_house.png";
+import HouseIcon from "../../Assets/Vector_house.png";
+import LayerIcon from "../../Assets/Vector_landlord.png";
+import EnvIcon from "../../Assets/Vector_env.png";
+import SafeIcon from "../../Assets/Vector_safe.png";
+import PayIcon from "../../Assets/Vector_pay.png";
 
 const Tenants = () => {
   return (
-    <div>Tenants Page</div>
-  )
-}
+    <div className="tenants_container">
+      <div className="about">
+        <h3>Minimum Living Cost Takes Care of Everything</h3>
+        <div className="sample">
+          <div className="sample_pic">
+            <img src={House} alt="sample house" />
+          </div>
+          <div className="house_att">
+            <div className="house_prop">
+              <img src={Vector} alt="coin" />
+              <p>Pay As Little As Possible</p>
+            </div>
+            <div className="house_prop">
+              <img src={HouseIcon} alt="house" />
+              <p>Enjoy Wisdom Of Community</p>
+            </div>
+            <div className="house_prop">
+              <img src={LayerIcon} alt="layer" />
+              <p>Lets Somebody Else Take Care Of Landlord</p>
+            </div>
+            <div className="house_prop">
+              <img src={EnvIcon} alt="env" />
+              <p>Enjoy Peaceful Environment</p>
+            </div>
+            <div className="house_prop">
+              <img src={SafeIcon} alt="sec" />
+              <p>
+                Stay Safe! <br /> Save Money
+              </p>
+            </div>
+            <div className="house_prop">
+              <img src={PayIcon} alt="sec" />
+              <p>Pay For What You Use!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="properties_container">
+        <h3>List of Properties</h3>
+        <div className="properties"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Tenants
+export default Tenants;
