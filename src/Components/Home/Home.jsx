342137b0b8d797776713a7a83dabece529b80133
ted <mairurat@gmail.com>
2023-01-24 15:33:26 +0300

    Home Page Desc

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./home.css";
 import map from "../../Assets/map.png";
 import { Link } from "react-router-dom";
 import logo from "../../Assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => {
   return (
@@ -34,16 +35,38 @@ const Home = () => {
               </Link>
             </li>
           </ul>
+          <p>Sign In</p>
         </nav>
       </div>
-      <div className="home_desc">
-        <h1>
-          The Most Affordable <br /> Place To Stay In <br />
-          Nairobi Area
-        </h1>
-      </div>
-      <div className="map">
-        <img src={map} alt="map" />
+      <div className="home_page">
+        <div className="home_desc">
+          <h1>
+            The Most Affordable <br /> Place To Stay In <br />
+            Nairobi Area
+          </h1>
+        </div>
+        <div className="map">
+          <img src={map} alt="map" />
+          <form>
+            <div className="location_box">
+              <select name="rooms" id="rooms">
+                <option value="">All Types</option>
+                <option value="1BR">1BR</option>
+                <option value="2BR">2BR</option>
+                <option value="3BR">3BR</option>
+              </select>
+              <select name="house" id="house">
+                <option value="">Neighborhood</option>
+                <option value="Kilimani">Kilimani</option>
+                <option value="Rongai">Rongai</option>
+                <option value="Kasarani">Kasarani</option>
+                <option value="Kahawa">Kahawa</option>
+              </select>
+              <input type="submit" value="Search" />
+              {/* <FontAwesomeIcon icon="fa-sharp fa-solid fa-magnifying-glass" /> */}
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
